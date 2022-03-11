abcdad5b40aee08484185451912b8ad191f80138
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2022-03-11 09:56:51 -0600

    pre-commit-terraform PR PoC

--- env.sh
+++ env.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-env
+set -eo pipefail
+
+# globals variables
+# hook ID, see `- id` for details in .pre-commit-hooks.yaml file
+# shellcheck disable=SC2155 # No way to assign to readonly variable in separate lines
+readonly SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+# shellcheck source=_common.sh
+. "$SCRIPT_DIR/pre-commit-terraform/hooks/_common.sh"
+
+function main {
+  common::initialize "$SCRIPT_DIR/pre-commit-terraform/hooks"
+  common::parse_cmdline "$@"
+  common::passthrough_env_vars
+}
+
+
+[ "${BASH_SOURCE[0]}" != "$0" ] || main "$@"
+echo "${ARGS}"
